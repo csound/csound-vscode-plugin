61d9a839eda7ee952d079d964d05c31aea2ef220
Forrest Cahoon <forrest.cahoon@gmail.com>
2019-10-16 17:44:09 -0500

    Don't try to kill running process when starting a new one; just kill all when requested

--- src/csoundCommands.ts
+++ src/csoundCommands.ts
@@ -6,7 +6,7 @@ import * as cp from 'child_process';
 import * as vscode from 'vscode';
 
 let output: vscode.OutputChannel;
-let process: cp.ChildProcess | undefined;
+const processMap: any = {};
 
 export async function playActiveDocument(textEditor: vscode.TextEditor) {
     const config = vscode.workspace.getConfiguration("csound");
@@ -29,9 +29,7 @@ export async function playActiveDocument(textEditor: vscode.TextEditor) {
     if (output === undefined) {
         output = vscode.window.createOutputChannel("Csound output");
     }
-    if (process !== undefined) {
-        killCsoundProcess();
-    }
+
     const command = config.get("executable", "csound");
     // We need to clone the args array because if we don't, when we push the filename on, it
     // will actually go into the config in memory, and be in the args of our next syntax check.
@@ -42,7 +40,10 @@ export async function playActiveDocument(textEditor: vscode.TextEditor) {
     output.clear();
     output.show();
 
-    process = cp.spawn(command, args, options);
+    const process = cp.spawn(command, args, options);
+
+    processMap[process.pid] = process;
+
     process.stdout.on('data', (data) => {
         // I've seen spurious 'ANSI reset color' sequences in some csound output
         // which doesn't render correctly in this context. Stripping that out here.
@@ -53,9 +54,8 @@ export async function playActiveDocument(textEditor: vscode.TextEditor) {
         // If you want your changes to show up, change this one.
         output.append(data.toString().replace(/\x1b\[m/g, ''));
     });
-    process.on('exit', () => { process = undefined });
     if (process.pid) {
-        console.log("Csound is playing");
+        console.log("Csound is playing (pid " + process.pid + ")");
     }
 }
 
@@ -89,8 +89,14 @@ async function setSaveSilentlyOnPlay() {
 }
 
 export function killCsoundProcess() {
-    if (process !== undefined) {
-        process.kill('SIGTERM');
-        console.log("Csound subprocess terminated");
+    for (let pid in processMap) {
+        let p = processMap[pid];
+        if (p === undefined) {
+            delete p[pid];
+        } else {
+            console.log("Killing Csound process (pid " + p.pid + ")");
+            p.kill('SIGTERM');
+            console.log("Csound subprocess terminated");
+        }
     }
 }
